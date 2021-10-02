b39fd2fcf84e65ab786a5678b7c25b7746803715
Vladislav <mvvminsk@gmail.com>
2021-10-02 17:04:15 +0300

    debug nav

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const navLi     = document.querySelector('.allNavA');
 const menuIcon  = document.querySelector('.menu-icon');
 
 navLi.style.maxHeight = '0px';
+navLi.style.display = 'none'
 
 menuIcon.onclick = function clickMenu(){
-
+    
     if( navLi.style.maxHeight == '0px' ){
         navLi.style.maxHeight = '130px';
+        if( navLi.style.maxHeight = '130px' ){
+            navLi.style.display = 'inline-table'
+        }
     }else{
         navLi.style.maxHeight = '0px'
+        navLi.style.display = 'none'
     }
 
 }
